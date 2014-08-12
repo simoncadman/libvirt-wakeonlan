10486ad538afad8352ba74486aa6e2738575f9e8
Simon Cadman <src@niftiestsoftware.com>
2014-08-12 19:09:11 +0100

    Fix syntax errors in test script

--- testing/full-test.sh
+++ testing/full-test.sh
@@ -19,27 +19,27 @@ py.test2 -rxs --cov-report xml  --cov . || py.test -rxs --cov-report xml  --cov 
 # start and stop daemon
 
 # gentoo
-if [ -f /etc/gentoo-release ] ; then \
-            /etc/init.d/libvirt-wakeonlan start \
-            /etc/init.d/libvirt-wakeonlan stop \
+if [ -f /etc/gentoo-release ] ; then
+            /etc/init.d/libvirt-wakeonlan start
+            /etc/init.d/libvirt-wakeonlan stop
 fi
 
 # redhat
-if [ -f /etc/redhat-release ] ; then \
-            service libvirt-wakeonlan start \
-            service libvirt-wakeonlan stop \
+if [ -f /etc/redhat-release ] ; then
+            service libvirt-wakeonlan start
+            service libvirt-wakeonlan stop
 fi
 
 # debian
-if [ -f /etc/debian-release || -f /etc/debian_version ] ; then \
-            service libvirt-wakeonlan start \
-            service libvirt-wakeonlan stop \
+if [ -f /etc/debian-release || -f /etc/debian_version ] ; then
+            service libvirt-wakeonlan start
+            service libvirt-wakeonlan stop
 fi
 
 # arch
-if [ -f /etc/arch-release ] ; then \
-            systemctl start libvirt-wakeonlan \
-            systemctl stop libvirt-wakeonlan \
+if [ -f /etc/arch-release ] ; then
+            systemctl start libvirt-wakeonlan
+            systemctl stop libvirt-wakeonlan
 fi
 
 exit 0
