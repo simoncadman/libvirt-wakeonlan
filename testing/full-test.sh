e0236fe47b216a7ce12eb0aa285885625ce5427f
Simon Cadman <src@niftiestsoftware.com>
2014-08-15 01:47:03 +0100

    Only check daemon status for now

--- testing/full-test.sh
+++ testing/full-test.sh
@@ -20,26 +20,22 @@ py.test2 -rxs --cov-report xml  --cov . || py.test -rxs --cov-report xml  --cov 
 
 # gentoo
 if [[ -f /etc/gentoo-release ]] ; then
-            /etc/init.d/libvirt-wakeonlan start
-            /etc/init.d/libvirt-wakeonlan stop
+            /etc/init.d/libvirt-wakeonlan status
 fi
 
 # redhat
 if [[ -f /etc/redhat-release ]] ; then
-            service libvirt-wakeonlan start
-            service libvirt-wakeonlan stop
+            service libvirt-wakeonlan status
 fi
 
 # debian
 if [[ -f /etc/debian-release || -f /etc/debian_version ]] ; then
-            service libvirt-wakeonlan start
-            service libvirt-wakeonlan stop
+            service libvirt-wakeonlan status
 fi
 
 # arch
 if [[ -f /etc/arch-release ]] ; then
-            systemctl start libvirt-wakeonlan
-            systemctl stop libvirt-wakeonlan
+            systemctl status libvirt-wakeonlan
 fi
 
 exit 0
